b9a962e2b71f9c945cf96aaf481616cb8c7a4a82
Jyoti Nigam <jyotinigam2370@gmail.com>
2024-03-10 22:49:49 +0530

    minor changes

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -19,25 +19,6 @@ export default function About() {
           </div>
         </div>
         <div className="col-span-6 place-self-center  my-8 lg-mt-0">
-          {/* <p className="text-justify">
-            An enthusiastic and results-driven professional with a strong
-            passion for machine learning and Python development, backed by 6
-            months of hands-on experience as a Data Engineer. Known for my
-            insightful contributions to the tech community through 12+ published
-            articles on GeeksforGeeks and a first prize-winning article on
-            CodeChef. My academic achievements include a silver medal and a
-            certificate of merit in B.Tech IT, as well as a commendable
-            performance in the Graduate Aptitude Test in Engineering (GATE).
-            Additionally, I hold a ”Sangeet Prabhakar” degree in Synthesizer and
-            have served as a Computer Science Subject Matter Expert at Chegg.
-            Currently, I am contributing to 4PointX Industrial Analytics LLP as
-            a Data Engineer, where I have excelled in enhancing code quality,
-            mentoring interns, and resolving critical technical issues. I have a
-            strong foundation in Python, Typescript, and a wide range of
-            developer tools and frameworks. Eager to leverage my skills and
-            knowledge in machine learning and Python to excel in challenging
-            roles and make a significant impact
-          </p> */}
           <p className="text-justify">
             An enthusiastic and results-driven with a strong passion for machine
             learning and Python development, backed by 6 months of hands-on
